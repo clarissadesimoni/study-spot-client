3b44c55974e7dcafb5885d21ac6c655960b8ce7a
Clarissa De Simoni <clarissa.de.simoni@gmail.com>
2023-07-05 12:59:01 +0200

    debugging tm closeTask

--- src/components/TaskComponent.js
+++ src/components/TaskComponent.js
@@ -2,8 +2,9 @@ import { useEffect, useState } from 'react';
 import { TodoistApi } from "@doist/todoist-api-typescript";
 import PropTypes from 'prop-types';
 import { Task } from '../classes';
+import { closeTask } from '../utilities/todoist';
 
-function TaskComponent({ obj, closeFunc }) {
+function TaskComponent({ token, obj }) {
     const [checked, setChecked] = useState(false);
 
     console.log(obj);
@@ -11,21 +12,21 @@ function TaskComponent({ obj, closeFunc }) {
     async function handleCheck() {
         setChecked(!checked);
         console('check');
-        await closeFunc(obj);
+        await closeTask(token, obj.id);
     }
 
     return (
         // handle edit name
         <>
-            <input type="checkbox" checked={checked} onchange={handleCheck} />
+            <input type="checkbox" onChange={handleCheck} />
             <text>{obj.name} - {obj.project_id} - {obj.due.toISOString()} - {obj.labels.reduce((acc, label) => acc + ',' + label, [])}</text>
         </>
     )
 }
 
 TaskComponent.propTypes = {
-    obj: PropTypes.instanceOf(Task).isRequired,
-    closeFunc: PropTypes.func.isRequired
+    token: PropTypes.string.isRequired,
+    obj: PropTypes.instanceOf(Task).isRequired
 }
 
 export default TaskComponent;
