0b193070feac99b5b1d1432a1b3bf50535640c6b
Clarissa De Simoni <clarissa.de.simoni@gmail.com>
2023-07-06 00:56:35 +0200

    debugging sign up

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -39,6 +39,10 @@ function Auth() {
     }
 
     async function signUp() {
+        console.log({
+            email: email,
+            password: password
+        });
         const { error } =  await supabase.auth.signUp({
             email: email,
             password: password
