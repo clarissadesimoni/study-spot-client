25bcc955aa04ffb83fe2bd37943c3f3aa68f16b2
Clarissa De Simoni <clarissa.de.simoni@gmail.com>
2023-07-06 02:22:20 +0200

    debugging tm createtask sup

--- src/components/TaskListView.js
+++ src/components/TaskListView.js
@@ -60,7 +60,7 @@ function TaskListView({ projects, labels, filters }) {
         const { error } = await supabase
         .from('tasks')
         .insert([
-        { title: newTaskName.current, projectId: newTaskProject.current, labels: newTaskLabels.current, due: (newTaskDue.current.toISOString()).toLocaleString('it-IT'), isCompleted: false, owner: session.user.id },
+        { title: newTaskName.current, projectId: projects.length == 0 ? projects[0].id : newTaskProject.current, labels: newTaskLabels.current, due: (newTaskDue.current.toISOString()).toLocaleString('it-IT'), isCompleted: false, owner: session.user.id },
         ]);
         if (error) {
             alert(error.message);
