b0b6c42697c405780070978b308e8a3fedb2c758
Clarissa De Simoni <clarissa.de.simoni@gmail.com>
2023-07-15 11:13:20 +0200

    supabase calendar

--- src/components/GoogleCalendarComponent.js
+++ src/components/GoogleCalendarComponent.js
@@ -14,7 +14,7 @@ import 'react-clock/dist/Clock.css';
 
 const DragAndDropCalendar = withDragAndDrop(Calendar);
 
-const localizer = momentLocalizer(moment.locale('it-IT'));
+const localizer = momentLocalizer(moment.locale('it'));
 
 function GoogleCalendarComponent() {
 
