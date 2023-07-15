3b04f029d0b5eb88f5cba48af323d0a8b6714b3b
Clarissa De Simoni <clarissa.de.simoni@gmail.com>
2023-07-15 17:44:20 +0200

    task due dates

--- src/classes/Task.js
+++ src/classes/Task.js
@@ -7,7 +7,7 @@ class Task {
         this.labels = labels;
         this.isCompleted = isCompleted;
         this.duration = duration;
-        this.due = !due_obj || !due_dict ? null : due_obj ?? new Date(Date.parse(due_dict.datetime ?? due_dict.date));
+        this.due = !due_obj && !due_dict ? null : due_obj ?? new Date(Date.parse(due_dict.datetime ?? due_dict.date));
     }
 
     isBefore = function (other) {
